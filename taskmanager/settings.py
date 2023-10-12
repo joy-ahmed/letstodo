5c0e11394b6bac80b1fa053f46e45479c0054c4f
joy-ahmed <dev.joyahmed@gmail.com>
2023-10-12 12:41:22 +0600

    settings file updated

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&kx+l=*z(9v$y&nq0lx4bucvjw^l6=7hp%tn_e)_t610fd6o)6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['letstodo-1ba134cc5c04.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
